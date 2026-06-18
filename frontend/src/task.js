20f1ca6b09906a783e2ecb3b3f97a648926272ac
utsav-kumar-droid <uku6396@gmail.com>
2026-06-18 10:46:33 +0530

    Describe your changes

--- frontend/src/task.js
+++ frontend/src/task.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { createRoot } from "react-dom/client";
 import axios from "axios";
-const API_BASE = import.meta.env.VITE_API_BASE;
+const API_BASE = process.env.API_BASE;
 
 // A built-in, lightweight electronic beep sound (Base64 string)
 // This eliminates the need for an external /alarm.mp3 file entirely!
